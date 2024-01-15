164030eddac3ed7960dc4d10ef92baf447bf1396
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-01-15 15:52:47 -0500

    Removed Slide Animation for Google Map View. Added toggle visibility for Google Maps on FAB click.

--- app/src/main/java/com/example/geoguesser/GuessPlaceActivity.kt
+++ app/src/main/java/com/example/geoguesser/GuessPlaceActivity.kt
@@ -2,25 +2,93 @@ package com.example.geoguesser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.geoguesser.databinding.ActivityGuessPlaceBinding
+import com.example.geoguesser.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback
 import com.google.android.gms.maps.StreetViewPanorama
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class GuessPlaceActivity : AppCompatActivity(), OnStreetViewPanoramaReadyCallback {
+class GuessPlaceActivity : AppCompatActivity(), OnStreetViewPanoramaReadyCallback,
+    OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityGuessPlaceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guess_place)
+        binding = ActivityGuessPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //StreetView Fragment
         val streetViewPanoramaFragment =
             supportFragmentManager
                 .findFragmentById(R.id.streetviewpanorama) as SupportStreetViewPanoramaFragment
         streetViewPanoramaFragment.getStreetViewPanoramaAsync(this)
+
+        //MapView Fragment
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map_Fragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        //Makes sure the map fragment is initially hidden until FAB is clicked
+        supportFragmentManager.beginTransaction()
+            .hide(mapFragment)
+            .commit()
+
+        //FAB used to toggle the visibility of the map fragment
+        binding?.mapFAB?.setOnClickListener {
+            Toast.makeText(this, "Floating Action Button Clicked!", Toast.LENGTH_SHORT).show()
+            toggleMapVisibility()
+
+        }
     }
 
     override fun onStreetViewPanoramaReady(streetViewPanorama: StreetViewPanorama) {
         val sanFrancisco = LatLng(37.754130, -122.447129)
         streetViewPanorama.setPosition(sanFrancisco)
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun toggleMapVisibility() {
+        val mapFragment: Fragment = supportFragmentManager.findFragmentById(R.id.map_Fragment)!!
+
+        if (!mapFragment.isAdded) {
+            // If map fragment is not added, add it and hide it
+            supportFragmentManager.beginTransaction()
+                .add(R.id.map_Fragment, mapFragment)
+                .hide(mapFragment)
+                .commit()
+        }
+
+        if (mapFragment.isVisible) {
+            // If map is visible, hide it
+            supportFragmentManager.beginTransaction()
+                .hide(mapFragment)
+                .commit()
+        } else {
+            // If map is hidden, show it
+            supportFragmentManager.beginTransaction()
+                .show(mapFragment)
+                .commit()
+        }
+    }
+
 
 }
